638ad040f2e9538ddac65d1fec3ecf5e988a8bd2
Indranil_Palit <73847100+IndranilPalit24@users.noreply.github.com>
2021-07-24 15:21:14 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Email_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Email em1 = new Email("Person1", "Person2");
+            Console.WriteLine(em1.Show_Info());
+        }
+    }
+}
